fa2f727fd1f296f73186ddf23bc72a9f662ff5c1
benweizhu <benweizhu@gmail.com>
2015-01-14 19:04:09 +0800

    [Relations-Front-End][Benwei] setup grunt env

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        jshint: {
+            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+            options: {
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    grunt.registerTask('default', ['jshint']);
+
+};
