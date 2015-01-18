84ca81f97def53324a59b9418942d30973b1891a
benweizhu <benweizhu@gmail.com>
2015-01-18 22:01:04 +0800

    [Relations-Front-End][Benwei] apply connect to start http server

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,12 +10,22 @@ module.exports = function (grunt) {
                     targetDir: './src/js/libs'
                 }
             }
+        },
+        connect: {
+            //to keep server alive, use command: grunt connect:server:keepalive
+            server: {
+                options: {
+                    port: 3000,
+                    base: 'src'
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-bower-task');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
-    grunt.registerTask('default', ['bower:install', 'jshint']);
+    grunt.registerTask('default', ['bower:install', 'jshint', 'connect']);
 
 };
