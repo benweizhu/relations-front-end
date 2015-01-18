d095bc057032dcb546e3fd75bb9702e960b258c0
benweizhu <benweizhu@gmail.com>
2015-01-18 18:07:28 +0800

    [Relations-Front-End][Benwei] run bower from grunt bower task plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,20 @@ module.exports = function (grunt) {
 
     grunt.initConfig({
         jshint: {
-            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
+            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js', '!src/js/libs/**/*.js']
+        },
+        bower: {
+            install: {
+                options: {
+                    targetDir: './src/js/libs'
+                }
+            }
         }
     });
 
+    grunt.loadNpmTasks('grunt-bower-task');
     grunt.loadNpmTasks('grunt-contrib-jshint');
 
-    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['bower:install', 'jshint']);
 
 };
