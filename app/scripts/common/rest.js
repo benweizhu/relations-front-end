1436a41ca7dd9585561d9a6fc2df6b44188c1516
benweizhu <benweizhu@gmail.com>
2015-04-06 10:34:37 +0800

    [relations-front-end][benwei] extract port and localhost need to switch between different environment

--- app/scripts/common/rest.js
+++ app/scripts/common/rest.js
@@ -1,15 +1,26 @@
 'use strict';
 
+var port = '8080';
+var localhost = 'http://localhost:' + port;
+
 angular.module('restApp', ['ngResource'])
     .factory('kitRest', ['$resource', function ($resource) {
-        return $resource('http://localhost:8080/relations-api/kits');
+        return $resource(localhost + '/relations-api/kits');
     }])
     .factory('locusRest', ['$resource', function ($resource) {
-        return $resource('http://localhost:8080/relations-api/kits/:kitId/loci');
+        return $resource(localhost + '/relations-api/kits/:kitId/loci');
     }])
     .factory('piRest', ['$resource', function ($resource) {
-        return $resource('http://localhost:8080/relations-api/pi/:action', {}, {
-            calculateParentsPi: {method: 'POST', params: { action: 'parents' }, isArray: false },
-            calculateOneParentPi: {method: 'POST', params: { action: 'oneparent' }, isArray: false }
+        return $resource(localhost + '/relations-api/pi/:action', {}, {
+            calculateParentsPi: {
+                method: 'POST',
+                params: { action: 'parents' },
+                isArray: false
+            },
+            calculateOneParentPi: {
+                method: 'POST',
+                params: { action: 'oneparent' },
+                isArray: false
+            }
         });
     }]);
