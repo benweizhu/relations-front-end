144e60121e2a57ef5fea238a5f41cd54cf4bd20e
benweizhu <benweizhu@gmail.com>
2015-04-06 10:31:03 +0800

    [relations-front-end][benwei] change rest url since api side already change

--- app/scripts/common/rest.js
+++ app/scripts/common/rest.js
@@ -2,13 +2,13 @@
 
 angular.module('restApp', ['ngResource'])
     .factory('kitRest', ['$resource', function ($resource) {
-        return $resource('http://localhost:8080/relations-with-gradle/kits');
+        return $resource('http://localhost:8080/relations-api/kits');
     }])
     .factory('locusRest', ['$resource', function ($resource) {
-        return $resource('http://localhost:8080/relations-with-gradle/locus?kit=:kit');
+        return $resource('http://localhost:8080/relations-api/kits/:kitId/loci');
     }])
     .factory('piRest', ['$resource', function ($resource) {
-        return $resource('http://localhost:8080/relations-with-gradle/pi/:action', {}, {
+        return $resource('http://localhost:8080/relations-api/pi/:action', {}, {
             calculateParentsPi: {method: 'POST', params: { action: 'parents' }, isArray: false },
             calculateOneParentPi: {method: 'POST', params: { action: 'oneparent' }, isArray: false }
         });
