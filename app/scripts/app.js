55080d8f6af04e123230b50dafea1fbe5886c0b6
benweizhu <benweizhu@gmail.com>
2015-04-14 21:43:01 +0800

    [relations-front-end][benwei] clean the useless css

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,27 +1,27 @@
 'use strict';
 
-angular
-    .module('relationsFrontEndApp', [
-        'ngResource',
-        'ngRoute'
-    ])
-    .config(function ($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'views/main.html',
-                controller: 'MainCtrl'
-            })
-            .when('/about', {
-                templateUrl: 'views/about.html',
-                controller: 'AboutCtrl'
-            })
-            .when('/one-parent', {
-                templateUrl: 'views/one-parent.html'
-            })
-            .when('/parents', {
-                templateUrl: 'views/parents.html'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    });
+var app = angular.module('relationsFrontEndApp', [
+    'ngResource',
+    'ngRoute'
+]);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl'
+        })
+        .when('/about', {
+            templateUrl: 'views/about.html',
+            controller: 'AboutCtrl'
+        })
+        .when('/parent', {
+            templateUrl: 'views/parent.html'
+        })
+        .when('/parents', {
+            templateUrl: 'views/parents.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
