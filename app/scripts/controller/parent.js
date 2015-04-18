7d79159a498adf1127f192928641de6358b99d59
benweizhu <benweizhu@gmail.com>
2015-04-18 23:50:46 +0800

    [relations-front-end][benwei] add validation for duplicate record

--- app/scripts/controller/parent.js
+++ app/scripts/controller/parent.js
@@ -20,10 +20,28 @@ angular.module('relations').controller('oneParentController', ['$scope', 'locusR
             $scope.records.splice(index, 1);
         };
 
+        $scope.locusName = function (locusId) {
+            var locusName = '';
+            _.each($scope.loci, function (locus) {
+                if (locus.locusId === locusId) {
+                    locusName = locus.name;
+                }
+            });
+            return locusName;
+        };
+
         $scope.addRecord = function () {
             piRest.calculateOneParentPi($scope.newRecord, function (response) {
-                    $scope.newRecord.pi = response.value;
-                    $scope.records.push($scope.newRecord);
+                    $scope.message = '';
+                    _.each($scope.records, function (record) {
+                        if (record.locus === $scope.newRecord.locus) {
+                            $scope.message = '该基因座已经存在';
+                        }
+                    });
+                    if (_.isEmpty($scope.message)) {
+                        $scope.newRecord.pi = response.value;
+                        $scope.records.push($scope.newRecord);
+                    }
                 }
             );
         };
