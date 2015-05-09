b83471102d6f643ea041615c59552b99b41609f9
benweizhu <benweizhu@gmail.com>
2015-05-09 19:20:33 +0800

    [relations-front-end][benwei] refactoring: change resource callback to promise

--- app/scripts/controller/parent.js
+++ app/scripts/controller/parent.js
@@ -3,13 +3,17 @@
 angular.module('relations').controller('parentCtrl', ['$scope', '$log', 'locusRest', 'kitRest', 'piRest',
     function ($scope, $log, locusRest, kitRest, piRest) {
 
-        $scope.records = [];
-        $scope.newRecord = {};
-        kitRest.query({}, function (response) {
-            $scope.kits = response;
-        });
-        $scope.cpi = 0;
-        $scope.rcp = 0;
+        function init() {
+            $scope.records = [];
+            $scope.newRecord = {};
+            kitRest.query({}, function (response) {
+                $scope.kits = response;
+            });
+            $scope.cpi = 0;
+            $scope.rcp = 0;
+        }
+
+        init();
 
         $scope.getLoci = function () {
             locusRest.query({kitId: $scope.kit.kitId}, function (success) {
@@ -27,15 +31,23 @@ angular.module('relations').controller('parentCtrl', ['$scope', '$log', 'locusRe
             return locusName;
         };
 
-        $scope.addRecord = function () {
-            $scope.message = '';
+        function rejectIfSameLocusExistInTable() {
             _.each($scope.records, function (record) {
                 if (record.locus === $scope.newRecord.locus) {
                     $scope.message = '基因座' + $scope.getLocusName($scope.newRecord.locus) + '已经存在';
                 }
             });
+        }
+
+        $scope.addRecord = function () {
+            $scope.message = '';
+            rejectIfSameLocusExistInTable();
             if (_.isEmpty($scope.message)) {
-                piRest.calculateOneParentPi($scope.newRecord, function (success) {
+
+                var piPromise = piRest.calculateOneParentPi($scope.newRecord).$promise;
+
+                var cpiPromise = piPromise.then(
+                    function (success) {
                         $scope.newRecord.pi = success.value;
                         $scope.records.push(_.clone($scope.newRecord));
 
@@ -43,15 +55,30 @@ angular.module('relations').controller('parentCtrl', ['$scope', '$log', 'locusRe
                         _.each($scope.records, function (record) {
                             pis.push(_.clone(record.pi));
                         });
-                        piRest.calculateCpi(pis, function (success) {
-                            $scope.cpi = success.value;
 
-                            piRest.calculateRcp($scope.cpi, function (success) {
-                                $scope.rcp = success.value;
-                            });
-                        });
+                        return piRest.calculateCpi(pis).$promise;
+
                     }, function (failure) {
                         $scope.message = '无法计算此PI值';
+                        $log.log(failure);
+                    }
+                );
+
+                var rcpPromise = cpiPromise.then(
+                    function (success) {
+                        $scope.cpi = success.value;
+                        return piRest.calculateRcp($scope.cpi).$promise;
+                    }, function (failure) {
+                        $scope.message = '无法计算此CPI值';
+                        $log.log(failure);
+                    }
+                );
+
+                rcpPromise.then(
+                    function (success) {
+                        $scope.rcp = success.value;
+                    }, function (failure) {
+                        $scope.message = '无法计算此RCP值';
                         $log.log(failure);
                     }
                 );
@@ -62,8 +89,7 @@ angular.module('relations').controller('parentCtrl', ['$scope', '$log', 'locusRe
             $scope.records.splice(index, 1);
         };
 
-        $scope.getArray = function () {
-            var exportedRecords = [];
+        function pushRecordToCsv(exportedRecords) {
             _.each($scope.records, function (record) {
                 var exportedRecord = _.clone(record);
                 _.each($scope.loci, function (locus) {
@@ -73,10 +99,14 @@ angular.module('relations').controller('parentCtrl', ['$scope', '$log', 'locusRe
                 });
                 exportedRecords.push(exportedRecord);
             });
+        }
+
+        $scope.exportRecordsToCSV = function () {
+            var exportedRecords = [];
+            pushRecordToCsv(exportedRecords);
             exportedRecords.push({cpiName: 'cpi', cpi: $scope.cpi});
             exportedRecords.push({rcpName: 'rcp', rcp: $scope.rcp});
             return exportedRecords;
         };
 
-    }])
-;
+    }]);
